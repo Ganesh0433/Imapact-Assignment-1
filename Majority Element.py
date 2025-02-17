71ae3dfefbdc8043b560d90037f0a5381642e4b3
Ganesh Reddy <137594208+Ganesh0433@users.noreply.github.com>
2025-02-17 13:14:15 +0530

    Create Majority Element.py

---
+++ Majority Element.py
@@ -0,0 +1,8 @@
+# Majority Element
+def majorityElement(nums):
+    count, candidate = 0, None
+    for num in nums:
+        if count == 0:
+            candidate = num
+        count += (1 if num == candidate else -1)
+    return candidate
