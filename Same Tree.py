94c5fd551691a49b4af16a26237a836043193ccf
Ganesh Reddy <137594208+Ganesh0433@users.noreply.github.com>
2025-02-17 13:24:03 +0530

    Create Same Tree.py

---
+++ Same Tree.py
@@ -0,0 +1,7 @@
+# Same Tree
+def isSameTree(p, q):
+    if not p and not q:
+        return True
+    if not p or not q or p.val != q.val:
+        return False
+    return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
